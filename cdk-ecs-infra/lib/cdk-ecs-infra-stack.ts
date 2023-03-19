9cf783353a7c92c2a6020636b474a6e3316980b9
winasaservice <pwafula@ymail.com>
2023-03-19 16:00:38 +0000

    cdk install and init, ec2 and ecs dependencies

---
+++ cdk-ecs-infra/lib/cdk-ecs-infra-stack.ts
@@ -0,0 +1,19 @@
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkEcsInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkEcsInfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
